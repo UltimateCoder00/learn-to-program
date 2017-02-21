fef0190d0b66a90ff4e00f70739dc5a2c315e7ea
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-02-21 15:37:05 +0000

    Added Chapter 3

---
+++ chap03/ex4.rb
@@ -0,0 +1,21 @@
+# ERRORS
+# puts '12' + 12
+# puts '2' * '5'
+
+# puts 'Betty' + 12
+# puts 'Fred' * 'John'
+# <TypeError: can't convert Fixnum into String>
+
+# ERRORS
+# puts 'You're swell!'
+
+# puts 'You're swell!'
+#             ^
+# syntax error, unexpected tIDENTIFIER, expecting end-of-input
+
+puts 'You\'re swell'
+
+puts 'You\'re swell!'
+puts 'backslash as the end of a string: \\'
+puts 'up\\down'
+puts 'up\down'
